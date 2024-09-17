302aa74ed7eaf365167ab1b453c6af05051ca098
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-09-17 19:47:57 -0300

    Renaming files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+string root = @"C:\Repositories\ShellScripts\";
+
+var files = from file in Directory.EnumerateFiles(root) select file;
+
+foreach (var file in files)
+{
+    if(!file.Contains(".sh"))
+        continue;
+    File.Move(file, $"{file}.txt");
+}
